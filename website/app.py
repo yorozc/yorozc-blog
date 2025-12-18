da9404ba883d4c0a60a43eba6cd3187b520de0d2
yorozc <yorozcolop49@gmail.com>
2025-12-18 15:27:17 -0800

    added basic paths and base.html

--- website/app.py
+++ website/app.py
@@ -1,7 +1,6 @@
-from website import main
 from main import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- website/main/__init__.py
+++ website/main/__init__.py
@@ -3,6 +3,15 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+
+    @app.route("/")
+    def index():
+        return "<h1>Hello</h1>"
+    
+    @app.route("/about")
+    def about():
+        return "<h1>About Page</h1>"
+
     return app
 
 

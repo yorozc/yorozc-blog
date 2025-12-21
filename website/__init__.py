8b25b9016a0d0ababd176754e1c6f339b5e09c3a
yorozc <yorozcolop49@gmail.com>
2025-12-21 09:47:37 -0800

    created routes.py and changed how proj is structured

--- website/__init__.py
+++ website/__init__.py
@@ -1,59 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask
 from flask_moment import Moment
 from datetime import datetime
 
-posts = [
-    {
-        'author': 'Yahir Orozco',
-        'title': 'Blog Post 1',
-        'content': 'First blog post I have done on this site',
-        'date_posted': datetime.now().strftime("%m-%d-%Y %I:%M%p")
-    },
-    {
-        'author': 'Yahir Orozco',
-        'title': 'Blog Post 2',
-        'content': 'Second blog post I have done on this site',
-        'date_posted': datetime.now().strftime("%m-%d-%Y %I:%M%p")
-    }
-]
+app = Flask(__name__)
+moment = Moment(app)
 
+from website import routes
 
-def create_app():
-    app = Flask(__name__)
-    moment = Moment(app)
-    
-
-    @app.route("/")
-    @app.route("/home")
-    def index():
-        return render_template("index.html", posts=posts, cur_time = datetime.now())
-    
-    @app.route("/about")
-    def about():
-        return render_template("about.html")
-    
-    # make a page to add a blog post
-    @app.route("/add_blog", methods=['POST', 'GET'])
-    def add_blog():
-        if request.method == "POST":
-            author = request.form["author"]
-            title = request.form["title"]
-            content = request.form["content"]
-            
-            post = {'author': author,
-                    'title': title, 
-                    'content': content,
-                    'date_posted': datetime.now().strftime("%m-%d-%Y %I:%M%p")}
-            
-            posts.append(post)
-
-            return redirect(url_for('index'))
-
-        elif request.method == "GET":
-
-            return render_template("add_blog.html")
-
-    return app
 
 
 

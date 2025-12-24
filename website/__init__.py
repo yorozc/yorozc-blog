b3b5ccd059c1c26dc73f8a6f38d6640d871b4877
yorozc <yorozcolop49@gmail.com>
2025-12-24 01:00:03 -0800

    connection tomongodb made

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,31 @@
 from flask import Flask
 from flask_moment import Moment
+from pymongo import MongoClient
+from pymongo.errors import ConfigurationError
+import os
 
 app = Flask(__name__)
 moment = Moment(app)
+
+try:
+    def _uri():
+        uri = os.getenv("MONGODB_URI")
+        if not uri:
+            raise ConfigurationError("MONGODB_URI not set")
+        return uri
+
+    def get_client() -> MongoClient:
+        return MongoClient(
+            _uri(),
+        )
+
+    def get_db():
+        return get_client()[os.getenv("DB")]
+
+    def get_users_collection():
+        return get_db()["COLLECTION"]
+except Exception as e:
+    print("Unable to make connection to DB")
 
 from website.main.routes import main
 from website.posts.routes import posts

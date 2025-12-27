586ebebfa7a0b6234d3cb52c85ed2c49f81c7a05
yorozc <yorozcolop49@gmail.com>
2025-12-27 11:36:15 -0800

    implemented an application factory for site

--- website/__init__.py
+++ website/__init__.py
@@ -7,35 +7,38 @@ from website.models.user import User
 from website.database.db import get_users_collection
 import os
 
-app = Flask(__name__)
-moment = Moment(app)
-bcrypt = Bcrypt(app)
+def create_app():
+    app = Flask(__name__)
+    moment = Moment(app)
+    bcrypt = Bcrypt(app)
 
-app.secret_key = os.getenv("SECRET_KEY")
+    app.secret_key = os.getenv("SECRET_KEY")
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app) 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app) 
 
-@login_manager.user_loader
-def load_user(id):
-    try:
-        _id = ObjectId(id)
+    @login_manager.user_loader
+    def load_user(id):
+        try:
+            _id = ObjectId(id)
 
-    except Exception:
-        return None
+        except Exception:
+            return None
     
-    users = get_users_collection()
-    doc = users.find_one({"_id": _id})
-    return User(doc) if doc else None
+        users = get_users_collection()
+        doc = users.find_one({"_id": _id})
+        return User(doc) if doc else None
 
-from website.main.routes import main
-from website.posts.routes import posts
-from website.users.routes import users
+    from website.main.routes import main
+    from website.posts.routes import posts
+    from website.users.routes import users
 
-app.register_blueprint(main)
-app.register_blueprint(posts)
-app.register_blueprint(users)
+    app.register_blueprint(main)
+    app.register_blueprint(posts)
+    app.register_blueprint(users)
+
+    return app
 
 
 
--- website/users/routes.py
+++ website/users/routes.py
@@ -2,7 +2,6 @@ from flask import (Blueprint, request, redirect, url_for, render_template,
                    flash)
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_bcrypt import bcrypt
-from website import bcrypt
 from website.database.db import get_users_collection
 from website.models.user import User
 

3a445ad4e1ca7e061760716711af42ac0baa9c79
yorozc <yorozcolop49@gmail.com>
2025-12-31 12:40:03 -0800

    added ids to blogs for searching but incomplete

--- website/posts/routes.py
+++ website/posts/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, abort
 from flask_login import login_required, current_user
 from datetime import datetime
+from uuid import uuid4
 from website.database.db import get_blog_collection
 
 posts = Blueprint('posts', __name__)
@@ -12,8 +13,10 @@ def add_blog():
         author = request.form["author"]
         title = request.form["title"]
         content = request.form["content"]
+        id = uuid4()
         
-        post = {'author': author,
+        post = {'blog_id': str(id),
+                'author': author,
                 'title': title, 
                 'content': content,
                 'date_posted': datetime.now().strftime("%m-%d-%Y %I:%M%p")
@@ -30,9 +33,28 @@ def add_blog():
         return render_template("add_blog.html")
     
 @login_required
+@posts.route("/blog/<blog_id>", methods=["GET"])
+def blog(blog_id):
+    coll = get_blog_collection()
+    blog = coll.find({"blog_id": blog_id})
+    if blog:
+        return render_template('blog.html', post=blog)
+    else:
+        abort(404)
+    
+@login_required
 @posts.route("/delete_blog", methods=["POST"])
-def delete():
-    pass
+def delete_blog(blog_id):
+    if request.method == "POST":
+        print(blog_id)
+        # # look for post via blog id
+        # post = get_blog_collection().find({"blog_id": blog_id})
+        # if post["author"] != current_user:
+        #     abort(403)
+        
+
+
+    return redirect(url_for("main.index"))
 
 @login_required
 @posts.route("/update_blog", methods=["POST"])
